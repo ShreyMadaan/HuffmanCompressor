571656f09f11246b78f2cf246d20840946436328
ShreyMadaan <89932358+ShreyMadaan@users.noreply.github.com>
2026-01-02 14:40:30 +0530

    feat: add file handler for reading and writing files

--- src/huffman/FileHandler.java
+++ src/huffman/FileHandler.java
@@ -1,4 +1,15 @@
 package huffman;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileHandler {
+    public static String readFile(String path) throws IOException {
+        return Files.readString(Path.of(path));
+    }
+
+    public static void writeFile(String path, String content) throws IOException {
+        Files.writeString(Path.of(path), content);
+    }
 }
